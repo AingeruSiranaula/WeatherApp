1092bb3ea2e1afe3f738d09cd15e53f25f747862
in2dm3 <aingerudaniel.siranaulasa@elorrieta-errekamari.com>
2023-10-18 08:24:28 +0500

    Agregar archivos de proyecto.

---
+++ Config.cs
@@ -0,0 +1,7 @@
+﻿namespace WeatherApp
+{
+    public class Config
+    {
+        public static string GeoApifyKey = "d3383ba0282d42c7bdb917554dae1e7e";
+    }
+}
---
+++ Utilities/WeatherCode.cs
@@ -0,0 +1,34 @@
+﻿namespace WeatherApp.Utilities
+{
+    public class WeatherCode
+    {
+        public static string Convert(int code)
+        {
+            switch (code)
+            {
+                case 0:
+                    return "Clear Sky";
+                    break;
+
+                case 1:
+                    return "Mainly clear";
+                    break;
+
+                case 2:
+                    return "Partly cloudy";
+                    break;
+
+                case 3:
+                    return "Overcast";
+                    break;
+
+                case 4:
+                    return "Thunderstorm with slight and heavy hail";
+                    break;
+
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
